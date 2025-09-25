c9df6ca46f2b0b87abd968ce5e1aa51bf328db3d
Warit <warit.ng@ku.th>
2025-09-25 15:23:47 +0700

    FSIAFRRH

--- assignments/week10/personal-validator.py
+++ assignments/week10/personal-validator.py
@@ -28,3 +28,64 @@ Age Group: Young Adult (18-30)
 Phone: +91-9876543210
 
 """
+
+print("=== PERSONAL INFORMATION VALIDATOR ===")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+phone = input("Enter your phone number: ")
+print()
+ 
+# name validate
+name_validator = True
+for char in name:
+    #print(char, char.isalpha())
+    if char.isalpha() == True or char == ".":
+        name_validator = True
+    else:
+        name_validator = False
+        break
+ 
+# age validate
+age_validator = True
+if 18 <= age and age <= 65:
+    age_validator == True
+else:
+    age_validator == False
+ 
+# phone number validate
+# 10 character
+phone_validator = True
+if len(phone) != 10:
+    phone_validator == False
+ 
+for char in phone:
+    if char.isdigit() == False:
+        phone_validator = False
+        break
+ 
+print()
+print("Validation Results:")
+if name_validator == True:
+    print("Name: Valid (contains only letters and spaces)")
+else:
+    print("Name: Invalid (not contains only letter and spaces)")
+ 
+if age_validator == True:
+    print(f"Age: Valid ({age} years old)")
+else:
+    print("Age: Invalid (less than 18 or more than 65)")
+ 
+if phone_validator == True:
+    print("Phone: Valid (10-digit number)")
+else:
+    print("Phone: Invalid (not 10-digit number)")
+ 
+print()
+print("Formatted Information:")
+print("Name: ", name.upper())
+if 18 <= age and age <= 30 :
+    print("Age Group: Young Adult (18-30)")
+else:
+    print("Age Group: Adult (>30)")
+print("Phone: +66-", phone)
+ 
